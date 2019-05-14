d22ade2fd25101dee6b21e246978275e8be939a2
AJigsawnHalo <nightlock0326@gmail.com>
2019-05-14 13:52:11 +0800

    added comments for easy tracking

--- mainlibrarywindow.cpp
+++ mainlibrarywindow.cpp
@@ -36,7 +36,7 @@ void MainLibraryWindow::on_issueHomeButton_clicked()
     ui->stackedWidget->setCurrentIndex(0);
 }
 
-void MainLibraryWindow::on_actionIssue_Book_triggered()
+void MainLibraryWindow::on_actionIssue_Book_triggered() //clears the Page
 {
     ui->stackedWidget->setCurrentIndex(1);
     ui->lineBookID->setText("");
@@ -59,7 +59,7 @@ void MainLibraryWindow::on_returnHomeButton_clicked()
     ui->stackedWidget->setCurrentIndex(0);
 }
 
-void MainLibraryWindow::on_homeReturnButton_clicked()
+void MainLibraryWindow::on_homeReturnButton_clicked() //clears the Page
 {
     ui->stackedWidget->setCurrentIndex(2);
     ui->lineBookID2->setText("");
@@ -71,7 +71,7 @@ void MainLibraryWindow::on_homeReturnButton_clicked()
     ui->linePubYear2->setText("");
 }
 
-void MainLibraryWindow::on_actionReturn_Book_triggered()
+void MainLibraryWindow::on_actionReturn_Book_triggered() //clears the Page
 {
     ui->stackedWidget->setCurrentIndex(2);
     ui->lineBookID2->setText("");
@@ -83,14 +83,14 @@ void MainLibraryWindow::on_actionReturn_Book_triggered()
     ui->linePubYear2->setText("");
 }
 
-void MainLibraryWindow::on_lineBookID_textChanged(const QString &arg1)
+void MainLibraryWindow::on_lineBookID_textChanged(const QString &arg1) //Queries the database for the book using the book ID
 {
     dbManager db;
     QString bookID = arg1;
     QSqlQuery query; //initialize the query
     query.prepare("SELECT * FROM BOOKS WHERE ID='"+bookID+"' ");
     if (query.exec()){
-        while (query.next()){
+        while (query.next()){ //This sets the text to the corresponding query
             ui->lineBookName->setText(query.value(1).toString());
             ui->lineAuthor->setText(query.value(3).toString());
             ui->lineCategory->setText(query.value(2).toString());
@@ -107,14 +107,14 @@ void MainLibraryWindow::on_lineBookID_textChanged(const QString &arg1)
 
 }
 
-void MainLibraryWindow::on_lineBookID2_textChanged(const QString &arg1)
+void MainLibraryWindow::on_lineBookID2_textChanged(const QString &arg1) //Queries the database for the book using the book ID
 {
     dbManager db;
     QString bookID = arg1;
     QSqlQuery query; //initialize the query
     query.prepare("SELECT * FROM BOOKS WHERE ID='"+bookID+"' ");
     if (query.exec()){
-        while (query.next()){
+        while (query.next()){ //This sets the text to the corresponding query
             ui->lineBookName2->setText(query.value(1).toString());
             ui->lineAuthor2->setText(query.value(3).toString());
             ui->lineCategory2->setText(query.value(2).toString());
@@ -130,7 +130,7 @@ void MainLibraryWindow::on_lineBookID2_textChanged(const QString &arg1)
     }
 }
 
-void MainLibraryWindow::on_refreshButton_2_clicked()
+void MainLibraryWindow::on_refreshButton_2_clicked() //clears the Page
 {
     ui->lineBookID2->setText("");
     ui->lineBookName2->setText("");
@@ -141,7 +141,7 @@ void MainLibraryWindow::on_refreshButton_2_clicked()
     ui->linePubYear2->setText("");
 }
 
-void MainLibraryWindow::on_refreshButton_clicked()
+void MainLibraryWindow::on_refreshButton_clicked() //clears the Page
 {
     ui->lineBookID->setText("");
     ui->lineBookName->setText("");
