36a6785464f205f3e17e25d951c05f630dee9d1b
AJigsawnHalo <nightlock0326@gmail.com>
2019-05-14 18:06:18 +0800

    added global variable class

---
+++ globals.cpp
@@ -0,0 +1,6 @@
+#include "globals.h"
+
+QString dbName = "aklatan.db";
+float damagePenalty = 150.0;
+float latePenalty = 150.0;
+QString penalty = "0.0";
---
+++ globals.h
@@ -0,0 +1,17 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <QtWidgets>
+
+QT_BEGIN_NAMESPACE
+class QString;
+QT_END_NAMESPACE
+
+extern QString dbName;
+extern float damagePenalty;
+extern float latePenalty;
+extern QString penalty;
+
+
+
+#endif // GLOBALS_H
