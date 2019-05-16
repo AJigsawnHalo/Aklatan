e0d7ad532c0d7ad8297b956d3d66788b027252a4
AJigsawnHalo <nightlock0326@gmail.com>
2019-05-17 00:31:58 +0800

    added a separate variable for each penalty

--- globals.cpp
+++ globals.cpp
@@ -3,7 +3,8 @@
 QString dbName = "aklatan.db";
 float damagePenalty = 150.0;
 float latePenalty = 150.0;
-QString penalty = "0.0";
+QString lpenalty;
+QString dpenalty;
 QDate today = QDate::currentDate();
 int daysDue = 3;
 QDate dueDate = today.addDays(daysDue);
