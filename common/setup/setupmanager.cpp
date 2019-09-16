061061937fb1d910b941315b26c8bed403e557be
Joash Christian Liwanag <nightlock0326@gmail.com>
2019-09-16 18:41:41 +0800

    Add Setup Function
    This will run if there is no existing .conf/.ini files at first run.

--- common/setup/setupmanager.cpp
+++ common/setup/setupmanager.cpp
@@ -6,9 +6,164 @@ SetupManager::SetupManager(QWidget *parent) :
     ui(new Ui::SetupManager)
 {
     ui->setupUi(this);
+	this->setWindowTitle("Aklatan - Setup");
 }
 
 SetupManager::~SetupManager()
 {
     delete ui;
+}
+
+// Start Page
+void SetupManager::on_buttonCancel0_clicked()
+{
+    QMessageBox::StandardButton reply;
+      reply = QMessageBox::question(this, "Quit", "Would You like to exit?",
+                                    QMessageBox::Yes|QMessageBox::No);
+      if (reply == QMessageBox::Yes) {
+
+        QApplication::quit();
+      }
+}
+
+void SetupManager::on_buttonNext0_clicked()
+{
+	this->setWindowTitle("Aklatan - Database Setup");
+	ui->stackedSetup->setCurrentIndex(1);
+	ui->stackedDB->setCurrentIndex(0);
+}
+
+// Database Setup
+void SetupManager::on_buttonBackdb1_clicked()
+{
+	this->setWindowTitle("Aklatan - Setup");
+	ui->stackedSetup->setCurrentIndex(0);
+}
+
+void SetupManager::on_buttonNextdb1_clicked()
+{
+	QString dbName = ui->linedbName->text();
+	setMan.saveSettings("DatabaseName", dbName, "Settings");
+	createDB();
+	ui->stackedDB->setCurrentIndex(1);
+}
+
+void SetupManager::on_buttonCanceldb1_clicked()
+{
+    QMessageBox::StandardButton reply;
+      reply = QMessageBox::question(this, "Quit", "Would You like to exit?",
+                                    QMessageBox::Yes|QMessageBox::No);
+      if (reply == QMessageBox::Yes) {
+
+        QApplication::quit();
+      }
+}
+
+// Database Setup - Page 2
+
+void SetupManager::on_buttonCanceldb2_clicked()
+{
+    QMessageBox::StandardButton reply;
+      reply = QMessageBox::question(this, "Quit", "Would You like to exit?",
+                                    QMessageBox::Yes|QMessageBox::No);
+      if (reply == QMessageBox::Yes) {
+
+        QApplication::quit();
+      }
+}
+
+void SetupManager::on_buttonBackdb2_clicked()
+{
+	ui->stackedDB->setCurrentIndex(0);
+}
+
+void SetupManager::createDB()
+{
+	setMan.loadConf();
+	setMan.setConf();
+	dbMan.connOpen();
+
+	QSqlQuery admin,books,category,damagedbooks,issued,logbook,users;	
+	admin.prepare("CREATE TABLE 'admin' ( 'user-id' INTEGER, 'username' TEXT, 'password' BLOB, PRIMARY KEY('user-id') )");
+	books.prepare( "CREATE TABLE 'books' ( 'BookID' INTEGER, 'BookName' BLOB, 'Category' BLOB, 'Author' BLOB, 'Publisher' BLOB, 'Edition' INTEGER, 'PublishingYear' INTEGER, 'Quantity' INTEGER, 'Status' TEXT, PRIMARY KEY('BookID') )");
+	category.prepare("CREATE TABLE 'category' ( 'ID' INTEGER, 'Category Name' BLOB )");
+	damagedbooks.prepare("CREATE TABLE 'damagedBooks' ( 'BookID' INTEGER, 'BookName' BLOB )");
+	issued.prepare("CREATE TABLE 'issued' ( 'BookID' INTEGER, 'BookName' BLOB, 'UserID' INTEGER, 'UserName' BLOB, 'UserType' BLOB, 'DateIssued' BLOB, 'DateDue' BLOB, 'TransactionNum' INTEGER, PRIMARY KEY('TransactionNum') )");
+	logbook.prepare("CREATE TABLE `logbook` ( `ID` INTEGER, `Name` BLOB, `Time` BLOB, `Date` BLOB, `Type` TEXT, `Action` TEXT )");
+	users.prepare("CREATE TABLE 'users' ( 'UserID' INTEGER, 'UserName' BLOB, 'UserAddress' BLOB, 'UserContactNumber' BLOB, 'UserType' BLOB )");
+	admin.exec();
+	books.exec();
+	category.exec();
+	damagedbooks.exec();
+	issued.exec();
+	logbook.exec();
+	users.exec();
+}
+
+void SetupManager::on_buttonNextdb2_clicked()
+{
+	QString uName,pass,passCheck;
+	uName = ui->lineUsername->text();
+	pass = ui->linePassword->text();
+	passCheck = ui->linePassCheck->text();
+
+	QSqlQuery query;
+	query.prepare("INSERT INTO admin (username,password) values ('"+uName+"', '"+pass+"')");
+
+	if (pass == passCheck){
+		query.exec();
+		ui->stackedSetup->setCurrentIndex(2);
+	}
+	else {
+        QMessageBox::about(this, "Password Error", "Passwords do not match.");
+		ui->linePassword->setText("");
+		ui->linePassCheck->setText("");
+	}
+}
+
+void SetupManager::on_buttonCancelSettings_clicked()
+{
+    QMessageBox::StandardButton reply;
+      reply = QMessageBox::question(this, "Quit", "Would You like to exit?",
+                                    QMessageBox::Yes|QMessageBox::No);
+      if (reply == QMessageBox::Yes) {
+
+        QApplication::quit();
+      }
+}
+
+void SetupManager::on_buttonBackSettings_clicked()
+{
+	ui->stackedSetup->setCurrentIndex(1);
+}
+
+void SetupManager::on_buttonNextSettings_clicked()
+{
+	QString due,late,damaged;
+
+	due = ui->lineDue->text();
+	late = ui->lineLate->text();
+	damaged = ui->lineDamaged->text();
+
+	QString group = "Settings";
+	QString key[3] = {"DaysDue", "LatePenalty", "DamagePenalty"};
+	QString conf[3] = {due, late, damaged};
+	for (int i = 0; i < 3; i++){
+       QString keyPass, confPass, defPass;
+       keyPass = key[i];
+       confPass = conf[i];
+       setMan.saveSettings(keyPass, confPass, group);
+	}
+	ui->stackedSetup->setCurrentIndex(3);
+}
+
+void SetupManager::on_buttonFinish_clicked()
+{
+	setMan.saveSettings("FirstRun", "False", "Settings");
+	this->hide();
+	setMan.loadConf();
+	setMan.setConf();
+	dbMan.connClose();
+	login.show();
+	login.dbCheck();
 }
--- common/setup/setupmanager.h
+++ common/setup/setupmanager.h
@@ -2,6 +2,11 @@
 #define SETUPMANAGER_H
 
 #include <QWidget>
+#include <QSqlQuery>
+#include <QMessageBox>
+#include "library-window/headers/loginform.h"
+#include "common/headers/dbmanager.h"
+#include "common/settings/settingsmanager.h"
 
 namespace Ui {
 class SetupManager;
@@ -15,8 +20,31 @@ public:
     explicit SetupManager(QWidget *parent = nullptr);
     ~SetupManager();
 
+private slots:
+	void on_buttonCancel0_clicked();
+	void on_buttonNext0_clicked();
+	
+	// Database Setup - Page 1
+	void on_buttonBackdb1_clicked();
+	void on_buttonNextdb1_clicked();
+	void on_buttonCanceldb1_clicked();
+	void createDB();
+	// Database Setup - Page 2
+	void on_buttonBackdb2_clicked();
+	void on_buttonNextdb2_clicked();
+	void on_buttonCanceldb2_clicked();
+	// Settings Setup
+	void on_buttonBackSettings_clicked();
+	void on_buttonNextSettings_clicked();
+	void on_buttonCancelSettings_clicked();
+	// Final Page
+	void on_buttonFinish_clicked();
+
 private:
     Ui::SetupManager *ui;
+	LoginForm login;
+	SettingsManager setMan;
+	dbManager dbMan;
 };
 
 #endif // SETUPMANAGER_H
