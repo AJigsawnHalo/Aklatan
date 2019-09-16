4c4986fcba3800359761a82a5f43a810d17f3cb6
Joash Christian Liwanag <nightlock0326@gmail.com>
2019-09-16 15:13:35 +0800

    Added a check for the first-time setup.
    The main function now checks for a setting called FirstRun to either run
    the Setup or continue to the Login Window.

--- library-window/cpp/main.cpp
+++ library-window/cpp/main.cpp
@@ -1,5 +1,6 @@
 #include "library-window/headers/loginform.h"
 #include "common/settings/settingsmanager.h"
+#include "common/setup/setupmanager.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -9,8 +10,17 @@ int main(int argc, char *argv[])
     a.setOrganizationName("Aklatan-project");
     a.setOrganizationDomain("https://github.com/ajigsawnhalo/aklatan");
     SettingsManager setMan;
-    setMan.setConf();
-    LoginForm w;
-    w.show();
-    return a.exec();
+	QString firstrun = setMan.loadSettings("FirstRun", "Settings", "true").toString(); 
+	qDebug() << firstrun;
+	if (firstrun == "true"){
+		SetupManager setup;
+		setup.show();	
+		return a.exec();
+	}
+	else {
+		setMan.setConf();
+		LoginForm w;
+		w.show();
+		return a.exec();
+	}
 }
